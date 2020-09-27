bd1d761c7054052d7bb4c55bc8ad185fa4111b2e
Aditya Pant <aditpant@gmail.com>
2020-09-27 12:31:49 +0530

    specified get route

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-// app.route('/').get(route.getReturn);
+app.route('/').get(route.getReturn);
 app.route('/').post(route.scrape);
 
 module.exports = app;
